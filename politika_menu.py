e7c211e11006ed2858b64ac790ea9e734e392a64
Zalan12 <farkaszalan@turr.hu>
2023-06-13 12:10:56 +0200

    kommit

--- politika_menu.py
+++ politika_menu.py
@@ -1,6 +1,7 @@
 from tkinter import*
 menu=Tk()
-logo=Canvas(menu,width=300,height=100, bg='white')
+logo=Canvas(menu,width=600,height=100, bg='grey')
 kep=PhotoImage(file='a_jologo-removebg-preview.png')
 item=logo.create_image(80,80,image=kep)
+logo.grid()
 menu.mainloop()
